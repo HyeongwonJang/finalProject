f95075c25c0aa190461d6438bea6939a81a8529b
신후 <@Kosta-HP>
2015-12-02 15:46:34 +0900

    진료기록VO 변경

--- animalhospital/src/main/java/org/animalhospital/treatment/model/VO/TreatmentRecordVO.java
+++ animalhospital/src/main/java/org/animalhospital/treatment/model/VO/TreatmentRecordVO.java
@@ -1,84 +1,99 @@
 package org.animalhospital.treatment.model.VO;
-import org.animalhospital.petowner.model.VO.MemberPetOwnerVO;
+import org.animalhospital.petowner.model.VO.PetVO;
 import org.animalhospital.vet.model.VO.HospitalVO;
+import org.animalhospital.vet.model.VO.VetLicenseVO;
 
 public class TreatmentRecordVO {
-	private String treatmentRecordNo;
+	private int treatmentRecordNo;
 	private String treatmentHours;
 	private String treatmentContent;
 	private double petWeight;
-	private MemberPetOwnerVO memberPetOwnerVO;
-	private HospitalVO hospitalVO;
+	private PetVO petVO;
+	private VetLicenseVO vetLicenseVO;
 	private DiseaseVO diseaseVO;
 	
 	public TreatmentRecordVO() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-	
-	public TreatmentRecordVO(String treatmentRecordNo, String treatmentHours,
-			String treatmentContent, double petWeight,
-			MemberPetOwnerVO memberPetOwnerVO, HospitalVO hospitalVO,
-			DiseaseVO diseaseVO) {
+
+	public TreatmentRecordVO(int treatmentRecordNo, String treatmentHours,
+			String treatmentContent, double petWeight, PetVO petVO,
+			VetLicenseVO vetLicenseVO, DiseaseVO diseaseVO) {
 		super();
 		this.treatmentRecordNo = treatmentRecordNo;
 		this.treatmentHours = treatmentHours;
 		this.treatmentContent = treatmentContent;
 		this.petWeight = petWeight;
-		this.memberPetOwnerVO = memberPetOwnerVO;
-		this.hospitalVO = hospitalVO;
+		this.petVO = petVO;
+		this.vetLicenseVO = vetLicenseVO;
 		this.diseaseVO = diseaseVO;
 	}
-	public String getTreatmentRecordNo() {
+
+	public int getTreatmentRecordNo() {
 		return treatmentRecordNo;
 	}
-	public void setTreatmentRecordNo(String treatmentRecordNo) {
+
+	public void setTreatmentRecordNo(int treatmentRecordNo) {
 		this.treatmentRecordNo = treatmentRecordNo;
 	}
+
 	public String getTreatmentHours() {
 		return treatmentHours;
 	}
+
 	public void setTreatmentHours(String treatmentHours) {
 		this.treatmentHours = treatmentHours;
 	}
+
 	public String getTreatmentContent() {
 		return treatmentContent;
 	}
+
 	public void setTreatmentContent(String treatmentContent) {
 		this.treatmentContent = treatmentContent;
 	}
+
 	public double getPetWeight() {
 		return petWeight;
 	}
+
 	public void setPetWeight(double petWeight) {
 		this.petWeight = petWeight;
 	}
-	public MemberPetOwnerVO getMemberPetOwnerVO() {
-		return memberPetOwnerVO;
+
+	public PetVO getPetVO() {
+		return petVO;
 	}
-	public void setMemberPetOwnerVO(MemberPetOwnerVO memberPetOwnerVO) {
-		this.memberPetOwnerVO = memberPetOwnerVO;
+
+	public void setPetVO(PetVO petVO) {
+		this.petVO = petVO;
 	}
-	public HospitalVO getHospitalVO() {
-		return hospitalVO;
+
+	public VetLicenseVO getVetLicenseVO() {
+		return vetLicenseVO;
 	}
-	public void setHospitalVO(HospitalVO hospitalVO) {
-		this.hospitalVO = hospitalVO;
+
+	public void setVetLicenseVO(VetLicenseVO vetLicenseVO) {
+		this.vetLicenseVO = vetLicenseVO;
 	}
+
 	public DiseaseVO getDiseaseVO() {
 		return diseaseVO;
 	}
+
 	public void setDiseaseVO(DiseaseVO diseaseVO) {
 		this.diseaseVO = diseaseVO;
 	}
+
 	@Override
 	public String toString() {
 		return "TreatmentRecordVO [treatmentRecordNo=" + treatmentRecordNo
 				+ ", treatmentHours=" + treatmentHours + ", treatmentContent="
-				+ treatmentContent + ", petWeight=" + petWeight
-				+ ", memberPetOwnerVO=" + memberPetOwnerVO + ", hospitalVO="
-				+ hospitalVO + ", diseaseVO=" + diseaseVO + "]";
+				+ treatmentContent + ", petWeight=" + petWeight + ", petVO="
+				+ petVO + ", vetLicenseVO=" + vetLicenseVO + ", diseaseVO="
+				+ diseaseVO + "]";
 	}
+	
 	
 	
 		
