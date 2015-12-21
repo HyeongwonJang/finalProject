3ed94942fb8cb60c2a48271980a78e9ee323c7c5
신후 <@Kosta-HP>
2015-12-21 11:31:36 +0900

    mistake

--- animalhospital/src/main/java/org/animalhospital/petowner/controller/PetOwnerController.java
+++ animalhospital/src/main/java/org/animalhospital/petowner/controller/PetOwnerController.java
@@ -37,7 +37,13 @@ public class PetOwnerController {
 			return "account/login_fail";
 		}
 	}
-	//로그아웃
+	
+	/**
+	 * 로그아웃
+	 * petOwner, vet 공통사항
+	 * @param request
+	 * @return
+	 */
 	@RequestMapping("logout.do")
 	public String logout(HttpServletRequest request){
 		HttpSession session = request.getSession(false);
@@ -46,60 +52,13 @@ public class PetOwnerController {
 		return "index";
 	}
 	
-	
-	//보호자 회원 정보 수정
-	@RequestMapping("updatePetOwner.do")
-	public String updatePetOwner(HttpServletRequest request,PetOwnerVO povo){
-		petOwnerService.updatePetOwner(povo);
-		request.getSession().setAttribute("loginVO", povo);
-		return "home_petOwner";
-	}
-	// 보호자 전화번호 중복 체크
-	@RequestMapping("telCheckAjax.do")
-	@ResponseBody
-	public String telCheckAjax(PetOwnerVO povo) {		
-		return petOwnerService.telCheckPetOwner(povo);
-	}
-	
-	//펫 등록
-		@RequestMapping(value = "registerPet.do")
-		public String registerPet(PetOwnerVO povo){
-			try {
-				petOwnerService.registerPet(povo);
-			} catch (Exception e) {
-				return "redirect:home.do";
-			}
-			return "redirect:registerPetResult.do?" + povo.getPetOwnerNo();
-		}
-		@RequestMapping("registerPetResult.do")
-		public String registerPetResult(){
-			return "account/register_pet_result";
-		}
-		
-		// 펫 정보 수정
-		@RequestMapping(value="findPetListById.do", method = RequestMethod.POST)
-		@ResponseBody
-		public PetOwnerVO findPetListById(HttpServletRequest request, PetOwnerVO povo){
-			if(povo.getPetOwnerId() == null){
-				povo = (PetOwnerVO) request.getSession(false).getAttribute("loginVO");
-				return petOwnerService.findPetListById(povo);
-			} else {
-				return petOwnerService.findPetListById(povo);
-			}
-		}	
-		@RequestMapping(value="findPetByPetName.do", method = RequestMethod.POST)
-		public ModelAndView findPetByPetName(PetOwnerVO povo){
-			return new ModelAndView("pet_update","povo",petOwnerService.findPetByPetName(povo));
-		}
-		@RequestMapping("updatePet.do")
-		public String updatePet(PetOwnerVO povo){
-			petOwnerService.updatePet(povo);
-			return "account/update_pet_result";
-		}
-	
-	//보호자 회원 가입
+	/**
+	 * 신규 보호자 회원가입
+	 * @param povo
+	 * @return
+	 */
 	@RequestMapping(value = "registerPetOwner.do", method = RequestMethod.POST)
-	public String register(PetOwnerVO povo) {
+	public String registerPetOwner(PetOwnerVO povo) {
 		try {
 			petOwnerService.registerPetOwner(povo);
 		} catch (Exception e) {
@@ -111,7 +70,12 @@ public class PetOwnerController {
 	public String registerPetOwnerResult(){
 		return "account/register_petowner_result";
 	}
-	//비회원 보호자 회원 가입
+	
+	/**
+	 * PET_OWNER table에 전화번호가 등록되어 있는 보호자 회원가입
+	 * @param povo
+	 * @return
+	 */
 	@RequestMapping(value = "registerPetOwnerByTel.do", method = RequestMethod.POST)
 	public String registerPetOwnerByTel(PetOwnerVO povo) {
 		try {
@@ -122,9 +86,95 @@ public class PetOwnerController {
 		return "redirect:registerPetOwnerResult.do";
 	}
 	
+	/**
+	 * 펫 등록
+	 * @param povo
+	 * @return
+	 */
+	@RequestMapping(value = "registerPet.do")
+	public String registerPet(PetOwnerVO povo){
+		try {
+			petOwnerService.registerPet(povo);
+		} catch (Exception e) {
+			return "redirect:home.do";
+		}
+		return "redirect:registerPetResult.do?" + povo.getPetOwnerNo();
+	}
+	@RequestMapping("registerPetResult.do")
+	public String registerPetResult(){
+		return "account/register_pet_result";
+	}
+	
+	/**
+	 * 보호자 회원 정보 수정
+	 * @param request
+	 * @param povo
+	 * @return
+	 */
+	@RequestMapping("updatePetOwner.do")
+	public String updatePetOwner(HttpServletRequest request,PetOwnerVO povo){
+		petOwnerService.updatePetOwner(povo);
+		request.getSession().setAttribute("loginVO", povo);
+		return "home_petOwner";
+	}
+	
+	/**
+	 * 펫 정보 수정
+	 * @param povo
+	 * @return
+	 */
+	@RequestMapping("updatePet.do")
+	public String updatePet(PetOwnerVO povo){
+		petOwnerService.updatePet(povo);
+		return "account/update_pet_result";
+	}
+	
+	/**
+	 * 보호자 회원가입시 전화번호 중복 체크
+	 * 전화번호가 존재하고 id가 null이라면 ok_update(비가입, 진료내역이 있음)
+	 * 전화번호가 존재하지 않는다면 ok(진료내역이 없는 신규)
+	 * 전화번호와 id가 모두 존재한다면 fail(가입한 회원의 전화번호)
+	 * @param povo
+	 * @return
+	 */
+	@RequestMapping("checkPetOwnerTelAjax.do")
+	@ResponseBody
+	public String checkPetOwnerTelAjax(PetOwnerVO povo) {		
+		return petOwnerService.checkPetOwnerTel(povo);
+	}
+	
+	/**
+	 * 보호자 회원가입시 petOwnerId 중복 체크
+	 * 같은 아이디가 존재하지 않으면 ok
+	 * 중복이면 fail
+	 * @param povo
+	 * @return
+	 */
 	@RequestMapping("findPetOwnerById.do")
 	@ResponseBody
 	public String findPetOwnerById(PetOwnerVO povo) {
 		return petOwnerService.findPetOwnerById(povo);
+	}
+	
+	@RequestMapping(value="findPetListById.do", method = RequestMethod.POST)
+	@ResponseBody
+	public PetOwnerVO findPetListById(HttpServletRequest request, PetOwnerVO povo){
+		if(povo.getPetOwnerId() == null){
+			povo = (PetOwnerVO) request.getSession(false).getAttribute("loginVO");
+			return petOwnerService.findPetListById(povo);
+		} else {
+			return petOwnerService.findPetListById(povo);
+		}
+	}	
+	
+	/**
+	 * 펫 정보 수정시 펫 리스트에서 선택된 petName과 session의 petOwnerNo를 이용하여
+	 * 펫 정보를 반환
+	 * @param povo
+	 * @return
+	 */
+	@RequestMapping(value="findPetByPetName.do", method = RequestMethod.POST)
+	public ModelAndView findPetByPetName(PetOwnerVO povo){
+		return new ModelAndView("pet_update","povo",petOwnerService.findPetByPetName(povo));
 	}
 }
